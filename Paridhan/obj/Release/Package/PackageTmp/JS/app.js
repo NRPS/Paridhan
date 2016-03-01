a4e92fa6c171c4382ae31267982a39063bd35bca
Prem Prakash <prem1014prakash@gmail.com>
2016-03-01 09:49:50 -0800

    added paridhan web app

---
+++ Paridhan/obj/Release/Package/PackageTmp/JS/app.js
@@ -0,0 +1,8 @@
+﻿
+define(['angularAMD'], function (angularAMD) {
+    var app = angular.module("paridhan", []);
+    app.controller('IndexCtrl', ['$scope', function ($scope) {
+        $scope.data = 'Hello Prem';
+    }])
+    return angularAMD.bootstrap(app);
+});
---
+++ Paridhan/obj/Release/Package/PackageTmp/main.js
@@ -0,0 +1,22 @@
+﻿require.config({
+    paths: {
+        'angular': 'bower/angular/angular.min',
+        'angularAMD': 'bower/angularAMD/angularAMD.min',
+        'jquery': 'bower/jquery/dist/jquery.min',
+        'bootstrap':'bower/bootstrap/dist/js/bootstrap.min'
+    },
+    shim: {
+        'angularAMD': {
+            deps:['angular']
+        },
+        'jquery': {
+
+        },
+        'bootstrap': {
+            deps: ['jquery']
+        }
+    }
+});
+define(['angularAMD', 'jquery', 'bootstrap', function (angularAMD, jquery, bootstrap) {
+
+}])
